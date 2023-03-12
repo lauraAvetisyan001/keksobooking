3a4303e4b7fac92242ec519c98e34e6e8a482e3f
mac <mac@MacBook-Pro-MacBook.local>
2023-03-12 16:10:12 +0200

    module1-task1

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+export const announcementInfo = {
+    titleArray: ['Something', 'Pri', 'Vetik'],
+    typeArray: ['house', 'flat', 'bungalow', 'palace'],
+    checkinArray: ['13:00', '14:00', '15:00'],
+    checkoutArray: ['13:00', '14:00', '15:00'],
+    featuresArray: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    descriptionArray: ['Something', 'about', 'house'],
+    photosArray: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+}
+
+export const announcementCount = 10;
